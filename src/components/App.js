36f7541725fc5bb00bca7959d21436cda8de5589
Shriram Rajeshwar Nawathe <108632713+ShriramNawathe@users.noreply.github.com>
2023-06-16 12:40:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,82 @@
 
-import React from "react";
+//import React from "react";
+//import './../styles/App.css';
+
+//const App = () => {
+ // return (
+   // <div>
+    //  {/* Do not remove the main div */}
+//  </div>
+// )
+//}
+
+//export default App
+
+
+import React, { useState, useEffect } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [timer, setTimer] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [laps, setLaps] = useState([]);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setTimer((prevTimer) => prevTimer + 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isRunning]);
+
+  const startTimer = () => {
+    setIsRunning(true);
+  };
+
+  const stopTimer = () => {
+    setIsRunning(false);
+  };
+
+  const resetTimer = () => {
+    setIsRunning(false);
+    setTimer(0);
+    setLaps([]);
+  };
+
+  const lapTimer = () => {
+    setLaps((prevLaps) => [...prevLaps, timer]);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <div className="timer">
+        <h1>{timer}</h1>
+        <div className="buttons">
+          {!isRunning ? (
+            <button onClick={startTimer}>Start</button>
+          ) : (
+            <button onClick={stopTimer}>Stop</button>
+          )}
+          <button onClick={resetTimer}>Reset</button>
+          <button onClick={lapTimer}>Lap</button>
+        </div>
+      </div>
+      <div className="laps">
+        <h2>Laps:</h2>
+        <ul>
+          {laps.map((lap, index) => (
+            <li key={index}>Lap {index + 1}: {lap}</li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
